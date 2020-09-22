f4c0ed2cb938a818ce4bf7eacb8f7638ea6ddbed
ADMIN <ADMIN@Academy.lan>
2020-09-22 14:42:05 -0500

    The Students Database App Outline

---
+++ StudentDatabaseApplication/src/studentdabaseapp/Student.java
@@ -0,0 +1,21 @@
+package studentdabaseapp;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private String gradeYear;
+	private int studentID;
+	private int tuitionBalance;
+	private int costOfCourse;
+	
+	// Constructor: Prompt user to enter students's name and year
+	
+	// Generate an ID
+	
+	// Enroll in courses
+	
+	// View balance
+	
+	// Show status
+
+}
---
+++ StudentDatabaseApplication/src/studentdabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,29 @@
+package studentdabaseapp;
+
+public class StudentDatabaseApp {
+
+	// Scenario: 
+	// You are a Database Administrator for a university
+	// You need to create an application to manage students enrollments and balance
+	
+	// You Application should do the following:
+	// Ask the user how many new students will be added to the database
+	// The user should be prompted to enter the name and year for each student
+	// The student should have a 5-digit unique ID, with the first number being their grade level
+	
+	// A student can enroll in the following course:
+	// History 101, Mathematics 101, English 101, Chemistry 101, Computer Science 101.
+	// Each course costs $600 to enroll
+	// The student should be able to view their balance and pay the tuition
+	
+	// To see the status of the student:
+	// We should see their name, ID, Courses enrolled and balance.
+	
+	public static void main(String[] args) {
+		// Ask how many users we want to add
+		
+		// Create n number of new students
+
+	}
+
+}
